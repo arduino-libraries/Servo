2fc1571df28b246e9ba90a4165c28f0e58901814
ensoniq <andrew.gregory.george@gmail.com>
2016-11-17 15:24:16 +0800

    Updated library to support twice as many servos
    With the use of the OCRxB (Output Compare Register B), and not resetting
    the Timer Counter after each refresh cycle, we can have another set of
    servos pulsed by sharing the one Timer Counter

--- src/avr/ServoTimers.h
+++ src/avr/ServoTimers.h
@@ -54,6 +54,7 @@ typedef enum { _timer3, _timer1, _Nbr_16timers } timer16_Sequence_t;
 
 #else  // everything else
 #define _useTimer1
-typedef enum { _timer1, _Nbr_16timers } timer16_Sequence_t;
+#define _useTimer1B
+typedef enum { _timer1, _timer1B, _Nbr_16timers } timer16_Sequence_t;
 #endif
 
