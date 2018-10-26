6a4c0af880629e27c1aeb7460fa21e27a3a2f1fb
JOHNS JOSEPH <35776080+DEVELOPER-IN-PROGRESS@users.noreply.github.com>
2018-10-26 19:52:06 +0530

    added serial print monitor  statements for

--- examples/Knob/Knob.ino
+++ examples/Knob/Knob.ino
@@ -16,6 +16,7 @@ int val;    // variable to read the value from the analog pin
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+Serial.begin(9600);
 }
 
 void loop() {
@@ -23,5 +24,7 @@ void loop() {
   val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
   myservo.write(val);                  // sets the servo position according to the scaled value
   delay(15);                           // waits for the servo to get there
+Serial.println(val) ;
+ Serial.print(" ") ;
 }
 
