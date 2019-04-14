f1e7bad0b74034f0f293cbf6be5bbde832c9866c
LukeMay35 <41739916+LukeMay35@users.noreply.github.com>
2019-04-14 12:14:02 +0200

    Indentation fix
    Indentation fix and index queue removed

--- src/Servo.h
+++ src/Servo.h
@@ -17,32 +17,32 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-/*
-  A servo is activated by creating an instance of the Servo class passing
+/* 
+  A servo is activated by creating an instance of the Servo class passing 
   the desired pin to the attach() method.
-  The servos are pulsed in the background using the value most recently
+  The servos are pulsed in the background using the value most recently 
   written using the write() method.
 
-  Note that analogWrite of PWM on pins associated with the timer are
+  Note that analogWrite of PWM on pins associated with the timer are 
   disabled when the first servo is attached.
-  Timers are seized as needed in groups of 12 servos - 24 servos use two
+  Timers are seized as needed in groups of 12 servos - 24 servos use two 
   timers, 48 servos will use four.
   The sequence used to sieze timers is defined in timers.h
 
   The methods are:
 
-	Servo - Class for manipulating servo motors connected to Arduino pins.
+    Servo - Class for manipulating servo motors connected to Arduino pins.
 
-	attach(pin )  - Attaches a servo motor to an i/o pin.
-	attach(pin, min, max  ) - Attaches to a pin setting min and max values in microseconds
-	default min is 544, max is 2400
-
-	write()     - Sets the servo angle in degrees.  (invalid angle that is valid as pulse in microseconds is treated as microseconds)
-	writeMicroseconds() - Sets the servo pulse width in microseconds
-	read()      - Gets the last written servo pulse width as an angle between 0 and 180.
-	readMicroseconds()   - Gets the last written servo pulse width in microseconds. (was read_us() in first release)
-	attached()  - Returns true if there is a servo attached.
-	detach()    - Stops an attached servos from pulsing its i/o pin.
+    attach(pin )  - Attaches a servo motor to an i/o pin.
+    attach(pin, min, max  ) - Attaches to a pin setting min and max values in microseconds
+    default min is 544, max is 2400  
+ 
+    write()     - Sets the servo angle in degrees.  (invalid angle that is valid as pulse in microseconds is treated as microseconds)
+    writeMicroseconds() - Sets the servo pulse width in microseconds 
+    read()      - Gets the last written servo pulse width as an angle between 0 and 180. 
+    readMicroseconds()   - Gets the last written servo pulse width in microseconds. (was read_us() in first release)
+    attached()  - Returns true if there is a servo attached. 
+    detach()    - Stops an attached servos from pulsing its i/o pin. 
  */
 
 #ifndef Servo_h
@@ -50,15 +50,15 @@
 
 #include <inttypes.h>
 
- /*
-  * Defines for 16 bit timers used with  Servo library
-  *
-  * If _useTimerX is defined then TimerX is a 16 bit timer on the current board
-  * timer16_Sequence_t enumerates the sequence that the timers should be allocated
-  * _Nbr_16timers indicates how many 16 bit timers are available.
-  */
+/* 
+ * Defines for 16 bit timers used with  Servo library 
+ *
+ * If _useTimerX is defined then TimerX is a 16 bit timer on the current board
+ * timer16_Sequence_t enumerates the sequence that the timers should be allocated
+ * _Nbr_16timers indicates how many 16 bit timers are available.
+ */
 
-  // Architecture specific include
+// Architecture specific include
 #if defined(ARDUINO_ARCH_AVR)
 #include "avr/ServoTimers.h"
 #elif defined(ARDUINO_ARCH_SAM)
@@ -89,39 +89,33 @@
 
 #if !defined(ARDUINO_ARCH_STM32F4)
 
-typedef struct {
-	uint8_t nbr : 6;             // a pin number from 0 to 63
-	uint8_t isActive : 1;             // true if this channel is enabled, pin not pulsed if false 
-} ServoPin_t;
+typedef struct  {
+  uint8_t nbr        :6 ;             // a pin number from 0 to 63
+  uint8_t isActive   :1 ;             // true if this channel is enabled, pin not pulsed if false 
+} ServoPin_t   ;  
 
 typedef struct {
-	ServoPin_t Pin;
-	volatile unsigned int ticks;
+  ServoPin_t Pin;
+  volatile unsigned int ticks;
 } servo_t;
-
-typedef struct {
-	volatile short int index[MAX_SERVOS];
-	volatile short int freePosition;
-	volatile short int actualPosition;
-} index_queue;
 
 class Servo
 {
 public:
-	Servo();
-	uint8_t attach(int pin);           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
-	uint8_t attach(int pin, int min, int max); // as above but also sets min and max values for writes. 
-	void detach();
-	void write(int value);             // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds 
-	void writeMicroseconds(int value); // Write pulse width in microseconds 
-	int read();                        // returns current pulse width as an angle between 0 and 180 degrees
-	int readMicroseconds();            // returns current pulse width in microseconds for this servo (was read_us() in first release)
-	bool attached();                   // return true if this servo is attached, otherwise false 
+  Servo();
+  uint8_t attach(int pin);           // attach the given pin to the next free channel, sets pinMode, returns channel number or 0 if failure
+  uint8_t attach(int pin, int min, int max); // as above but also sets min and max values for writes. 
+  void detach();
+  void write(int value);             // if value is < 200 its treated as an angle, otherwise as pulse width in microseconds 
+  void writeMicroseconds(int value); // Write pulse width in microseconds 
+  int read();                        // returns current pulse width as an angle between 0 and 180 degrees
+  int readMicroseconds();            // returns current pulse width in microseconds for this servo (was read_us() in first release)
+  bool attached();                   // return true if this servo is attached, otherwise false 
 private:
-	uint8_t servoIndex;               // index into the channel data for this servo
-	int8_t min;                       // minimum is this value times 4 added to MIN_PULSE_WIDTH    
-	int8_t max;                       // maximum is this value times 4 added to MAX_PULSE_WIDTH   
-	bool firstWrite;					 // need to know if this is the first write
+   uint8_t servoIndex;               // index into the channel data for this servo
+   int8_t min;                       // minimum is this value times 4 added to MIN_PULSE_WIDTH    
+   int8_t max;                       // maximum is this value times 4 added to MAX_PULSE_WIDTH   
+   bool firstWrite;                  // need to know if is the first write
 };
 
 #endif
