1ad711c72c8376c7b0cdf1b45a03782791f8ba55
KAbhijeet2105 <ajayaish5@gmail.com>
2020-03-12 21:30:12 +0530

    Auto Formatting

--- examples/Random_Rotate/Random_Rotate/Random_Rotate.ino
+++ examples/Random_Rotate/Random_Rotate/Random_Rotate.ino
@@ -1,8 +1,8 @@
 /* Rotate
- by K.Abhijeet 
- This example code rotates the servo motor at random angle.
+  by K.Abhijeet
+  This example code rotates the servo motor at random angle.
 
- Circuit:-
+  Circuit:-
     Arduino Uno
     Micro Servo Motor connected at pin no 9
 
@@ -10,22 +10,22 @@
 
 #include<Servo.h>
 
-Servo Smotor;           // Servo motor object 
- int pos=0;
+Servo Smotor;           // Servo motor object
+int pos = 0;
 
 
 void setup() {
-     Serial.begin(9600);
-     Smotor.attach(9);            //Servo Motor connected at pin no 9
-      
-     Smotor.write(0);
+  Serial.begin(9600);
+  Smotor.attach(9);            //Servo Motor connected at pin no 9
+
+  Smotor.write(0);
 }
 
 void loop() {
 
-           pos= random(0,180);                           //calculates random angle
-           Smotor.write(pos);
-           Serial.print("Current Motor position is:");   
-           Serial.println(pos);                         //prints motor angle
-           delay(1000);                                 //Wait for 1 second
+  pos = random(0, 180);                         //calculates random angle
+  Smotor.write(pos);
+  Serial.print("Current Motor position is:");
+  Serial.println(pos);                         //prints motor angle
+  delay(1000);                                 //Wait for 1 second
 }
