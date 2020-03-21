a03a595b28f38760149f452ebcc22b3f5d9cd2fc
Madhur Dixit <dmadhur6@gmail.com>
2020-03-21 16:48:00 +0530

    Requested changes done

--- examples/ServoStop/ServoStop.ino
+++ examples/ServoStop/ServoStop.ino
@@ -1,36 +1,34 @@
 #include <Servo.h>
-#define trigPin  4
-#define echoPin  2
+byte trigPin = 4;
+byte echoPin = 2;
+byte servoPin = 9;
 
 Servo myservo;  // create servo object to control a servo
 int pos = 0; // variable to store the servo position
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(servoPin);  // attaches the Servo on pin 9 to the servo object
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  long duration, distance;
+  unsigned long duration;
+  unsigned int distance;
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
   distance = (duration / 2) / 29.1;
-  digitalWrite(trigPin, LOW);
 
+  Serial.print("Distance: ");
   Serial.print(distance);
-  Serial.print("\n");
+  Serial.println(" cm");
 
-  if (distance < 17)
+  if (distance < 17) {
     myservo.write(0);   // tell servo to go to position 0
-  else
+  } else {
     myservo.write(90);   // tell servo to go to position 90
-
-
-
+  }
 }
