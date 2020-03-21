0a6754d533556a62d278a4a3804ca66ce5dd7c49
Hamza Khalid <36852564+hmzakhalid@users.noreply.github.com>
2020-03-21 13:13:10 +0500

    Fixed input stream issues.
    Removed random capitalization.
    Removed random line spaces.
    Fixed Input from the serial moniter.

--- examples/Input/Input.ino
+++ examples/Input/Input.ino
@@ -1,15 +1,13 @@
 /*
  Controlling a servo position using serial input
  by Hamza Khalid https://github.com/hmzakhalid
-
 */
 
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
 
-
-int pos = 0;    // initial Position of the Shaft (max is 180)
+long pos = 0;    // initial position of the shaft (max is 180)
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
@@ -17,12 +15,9 @@ void setup() {
   Serial.println("Enter Position from 1 to 180"); 
 }
 
-
 void loop() {
-  
-  if(Serial.available() > 0){                       // checks if there is any input on the serial monitor
-    pos = Serial.read();               // Reading the input from the serial monitor and storing it in'pos'
+  if(Serial.available() > 0){        // checks if there is any input on the serial monitor
+    pos = Serial.parseInt();         // Reading the input from the serial monitor and storing it in'pos'
     myservo.write(pos);              // tell servo to go to that position 
-    delay(15);                       // waits 15ms for the servo to reach the position
   }
 }
