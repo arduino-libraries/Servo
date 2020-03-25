881950227a4ae972a2d529b21e03c4ddbcb2cd21
Divyansh <divyanshkhandelwal147@gmail.com>
2020-03-25 23:12:56 +0530

    bug fix for both Newline and carriage return

--- examples/Calibrate/Calibrate.ino
+++ examples/Calibrate/Calibrate.ino
@@ -1,5 +1,5 @@
 /* Calibrate
-  This code allows to calibrate your servo motor using the serial monitor
+  This code allows you to calibrate your servo motor using the serial monitor
   Enter the degree in the serial monitor
   by Divyansh Khandelwal <github.com/noob-master147>
   This example code is in the public domain.
@@ -23,11 +23,12 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     input = Serial.parseInt();        // reads the value from Serial
+    delay(25);
     while (Serial.available()) {      // remove the line endings
       Serial.read();
     }
     myservo.write(input);             // sets the servo position
-    delay (50);
+    delay (25);
     Serial.print("Servo set to ");
     Serial.print(input);
     Serial.println(" degree");
